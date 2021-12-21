4719b2a6c05807329573e5b74b796fe3aab0ee76
Prema Chhaya <prema.chhaya@gmail.com>
2021-12-21 06:42:01 +0530

    rendering logic simplified

--- src/components/EngagementList/EngagementList.jsx
+++ src/components/EngagementList/EngagementList.jsx
@@ -1,69 +1,35 @@
 
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import Engagement from '../Engagement/Engagement';
 import EngagementDetail from '../EngagementDetail/EngagementDetail';
 
 const EngagementList = (props) => {
-    const { engagamentList, header} = props;
+    const { engagamentList, header } = props;
 
-    const [ showEngagementDetail, setShowEngagementDetail ] = useState(false);
+    const [showEngagementDetail, setShowEngagementDetail] = useState(false);
 
     const [engagementToView, setEngagementToView] = useState({});
-    console.log("in engagement list");
-
-   console.log(engagamentList);
-
-    const categoryArray = ["professional", "creative", "sports", "technical", "educational", "domestic", "research"];
-    const categoryHeaders = ["Professional", "Creative", "Sports", "Technical", "Educational", "Domestic", "Research"];
-
-    const filteredCategoryEngagements = [];
-    for (let i = 0; i < categoryArray.length; i++) 
-    {
-         filteredCategoryEngagements[i] = engagamentList.filter(engagement => engagement.category === categoryArray[i]);
-        
-    };
-
-  //  console.log(filteredCategoryEngagements);
-
-    const renderEngagements = (engagements, headers) => {
-
-    
-
-        const columns = headers.map( (eachCategory, index) => (
-
-            <section>
-                {/* <h3 className="engagemenr__category">{eachCategory}</h3> */}
-
-                {showEngagementDetail && <EngagementDetail engagementToView={engagementToView} showEngagementDetail={showEngagementDetail} setShowEngagementDetail={setShowEngagementDetail}/>}
-                { (eachCategory) && engagements[index].map ( (engagement, index1) => {
-
-                    return (
-
-                        <Engagement className="engagement" key={engagement.id+engagement.preferredId} index={index1} engagement={engagement} header={header} 
-                        showEngagementDetail={showEngagementDetail} setShowEngagementDetail={setShowEngagementDetail}
-                        
-                        engagementToView={engagementToView} setEngagementToView={setEngagementToView}  />
-                    )
-
-                     } ) }
-
-            </section>
-        ));
-      
-        console.log(columns);
-        return columns;
- }
-
 
     return (
 
-     <>
-     <section className="engagement__detail">  
+        <>
+            <section className="engagement__detail">
 
-         {renderEngagements(filteredCategoryEngagements,categoryHeaders)}
+                {showEngagementDetail && <EngagementDetail engagementToView={engagementToView} showEngagementDetail={showEngagementDetail} setShowEngagementDetail={setShowEngagementDetail} />}
+                {engagamentList.map((engagement, index) => {
 
-     </section> 
-     </> 
+                    return (
+
+                        <Engagement className="engagement" key={engagement.id + engagement.preferredId} index={index} engagement={engagement} header={header}
+                            showEngagementDetail={showEngagementDetail} setShowEngagementDetail={setShowEngagementDetail}
+
+                            engagementToView={engagementToView} setEngagementToView={setEngagementToView} />
+                    )
+
+                })}
+
+            </section>
+        </>
     )
 }
 
